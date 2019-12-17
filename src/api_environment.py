aeb9c1a3c274afcd8dcffca6f0d30e378b0204a8
Narek.Andreasyan <Narek.Andreasyan@unifr.ch>
2019-12-17 12:59:40 +0100

    - small fixes

--- src/api_environment.py
+++ src/api_environment.py
@@ -15,15 +15,16 @@ def get_plants(request):
 
 
 def get_plant(request):
-    result = mysql.select_plant_by_id(request.match_info['id'])
-    if result == None: return web.json_response("Plant not found", status=404)
+    result = mysql.select_plant_by_id(request.match_info["id"])
+    if result == None:
+        return web.json_response("Plant not found", status=404)
     return web.json_response(result, status=200)
 
 
 def delete_plant(request):
-    result = mysql.select_plant_by_id(request.match_info['id'])
+    result = mysql.select_plant_by_id(request.match_info["id"])
     if result:
-        mysql.del_plant_by_id(request.match_info['id'])
+        mysql.delete_plant_by_id(request.match_info["id"])
         return web.json_response("Plant successfully deleted", status=200)
     else:
         return web.json_response("Plant not found ", status=404)
@@ -31,9 +32,27 @@ def delete_plant(request):
 
 async def post_plants(request):
     data = await request.json()
-    mysql.insert_plant(data['name'], data['nb_sunny_days'], data['nb_rainy_days'], data['watering_interval_days'],
-                       data['thing_id'])
-    return web.json_response(data, status=201)
+    plant = mysql.insert_plant(
+        data["name"],
+        data["nb_sunny_days"],
+        data["nb_rainy_days"],
+        data["watering_interval_days"],
+        data["thing_id"],
+    )
+    return web.json_response(plant, status=201)
+
+
+async def put_plant(request):
+    data = await request.json()
+    plant = mysql.update_plant(
+        request.match_info["id"],
+        data["name"],
+        data["nb_sunny_days"],
+        data["nb_rainy_days"],
+        data["watering_interval_days"],
+        data["thing_id"],
+    )
+    return web.json_response(plant, status=201)
 
 
 # THINGY API METHODS
@@ -44,41 +63,52 @@ def get_things(request):
 
 
 def get_thing(request):
-    result = mysql.select_thing_by_id(request.match_info['id'])
-    if result == None: return web.json_response("Thing not found", status=404)
+    result = mysql.select_thing_by_id(request.match_info["id"])
+    if result == None:
+        return web.json_response("Thing not found", status=404)
     return web.json_response(result, status=200)
 
 
 def get_thing_properties(request):
     results = {}
-    thing = mysql.select_thing_by_id(request.match_info['id'])
-    if thing == None: return web.json_response("Thing not found", status=404)
-    if request.query_string.split("&") == ['']:
+    thing = mysql.select_thing_by_id(request.match_info["id"])
+    if thing == None:
+        return web.json_response("Thing not found", status=404)
+    if request.query_string.split("&") == [""]:
         for property in mysql.select_properties():
-            results[property['name']] = influx.get_thingy_characteristic(thing['mac_address'],
-                                                                         property['characteristic'])
+            results[property["name"]] = influx.get_thingy_characteristic(
+                thing["mac_address"], property["characteristic"]
+            )
     else:
         params = dict(x.split("=") for x in request.query_string.split("&"))
         for property in mysql.select_properties():
-            results[property['name']] = get_filter_data(thing['mac_address'], property['characteristic'], params)
+            results[property["name"]] = get_filter_data(
+                thing["mac_address"], property["characteristic"], params
+            )
     return web.json_response(results, status=200)
 
 
 def get_thing_property(request):
-    thing = mysql.select_thing_by_id(request.match_info['id'])
-    if thing == None: return web.json_response("Thing not found", status=404)
-    property = mysql.select_property_by_name(request.match_info['name'])
-    if property == None: return web.json_response("Property not found", status=404)
-    if request.query_string.split("&") == ['']:
-        result = influx.get_thingy_characteristic(thing['mac_address'], property['characteristic'])
+    thing = mysql.select_thing_by_id(request.match_info["id"])
+    if thing == None:
+        return web.json_response("Thing not found", status=404)
+    property = mysql.select_property_by_name(request.match_info["name"])
+    if property == None:
+        return web.json_response("Property not found", status=404)
+    if request.query_string.split("&") == [""]:
+        result = influx.get_thingy_characteristic(
+            thing["mac_address"], property["characteristic"]
+        )
     else:
         params = dict(x.split("=") for x in request.query_string.split("&"))
-        result = get_filter_data(thing['mac_address'], property['characteristic'], params)
+        result = get_filter_data(
+            thing["mac_address"], property["characteristic"], params
+        )
     return web.json_response(result, status=200)
 
 
 async def put_thing_property(request):
-    thing_id = request.match_info['id']
+    thing_id = request.match_info["id"]
     property = await request.json()
     # TODO
     return get_thing_property(request)
@@ -91,8 +121,8 @@ def get_thing_events(request):
 
 
 def get_thing_event(request):
-    thing_id = request.match_info['id']
-    event_type = int(request.match_info['type'])
+    thing_id = request.match_info["id"]
+    event_type = int(request.match_info["type"])
     event = [event_type]
     # TODO
     return web.json_response(event)
@@ -107,27 +137,31 @@ def get_filter_data(thingy_mac, characteristic_name, params):
     startHour = None
     endHour = None
     try:
-        date = params['date']
+        date = params["date"]
         validate_date(date)
     except ValueError:
         raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
     try:
-        startHour = params['startHour']
-        endHour = params['endHour']
+        startHour = params["startHour"]
+        endHour = params["endHour"]
     except:
         filterByHours = False
 
     # Get data from influx db
     if filterByHours:
-        return list(influx.get_characteristic_by_hours(characteristic_name, date, startHour, endHour, thingy_mac))
+        return list(
+            influx.get_characteristic_by_hours(
+                characteristic_name, date, startHour, endHour, thingy_mac
+            )
+        )
 
     return list(influx.get_characteristic_by_day(characteristic_name, date, thingy_mac))
 
 
 def validate_date(date):
     try:
-        datetime.datetime.strptime(date, '%Y-%m-%d')
+        datetime.datetime.strptime(date, "%Y-%m-%d")
     except ValueError:
         raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
@@ -143,26 +177,37 @@ async def app_factory(args=()):
     app = web.Application()
 
     # Configure default CORS settings.
-    cors = aiohttp_cors.setup(app,
-                              defaults={"*": aiohttp_cors.ResourceOptions(allow_credentials=True, expose_headers="*",
-                                                                          allow_headers="*")})
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True, expose_headers="*", allow_headers="*"
+            )
+        },
+    )
 
     # Resources
-    plants_resource = cors.add(app.router.add_resource("/plants", name='plants'))
+    plants_resource = cors.add(app.router.add_resource("/plants", name="plants"))
     cors.add(plants_resource.add_route("GET", get_plants))
     cors.add(plants_resource.add_route("POST", post_plants))
-    plant_resource = cors.add(app.router.add_resource("/plant/{id:\\d+}", name='plant'))
+    plant_resource = cors.add(app.router.add_resource("/plant/{id:\\d+}", name="plant"))
     cors.add(plant_resource.add_route("GET", get_plant))
     cors.add(plant_resource.add_route("DELETE", delete_plant))
-    things_resource = cors.add(app.router.add_resource("/things", name='things'))
+    cors.add(plant_resource.add_route("PUT", put_plant))
+
+    things_resource = cors.add(app.router.add_resource("/things", name="things"))
     cors.add(things_resource.add_route("GET", get_things))
-    thing_resource = cors.add(app.router.add_resource("/thing/{id:\\d+}", name='thing'))
+    thing_resource = cors.add(app.router.add_resource("/thing/{id:\\d+}", name="thing"))
     cors.add(thing_resource.add_route("GET", get_thing))
     thing_properties_resource = cors.add(
-        app.router.add_resource("/thing/{id:\\d+}/properties", name='thing_properties'))
+        app.router.add_resource("/thing/{id:\\d+}/properties", name="thing_properties")
+    )
     cors.add(thing_properties_resource.add_route("GET", get_thing_properties))
     thing_property_resource = cors.add(
-        app.router.add_resource("/thing/{id:\\d+}/property/{name}", name='thing_property'))
+        app.router.add_resource(
+            "/thing/{id:\\d+}/property/{name}", name="thing_property"
+        )
+    )
     cors.add(thing_property_resource.add_route("GET", get_thing_property))
     # cors.add(thing_property_resource.add_route("PUT", put_thing_property))
     # thing_events_resource = cors.add(app.router.add_resource("/thing/{id:\d+}/events/", name='thing_events'))
@@ -173,5 +218,5 @@ async def app_factory(args=()):
     return app
 
 
-if __name__ == '__main__':
-    run_app(app_factory(), host='0.0.0.0', port=8081)
+if __name__ == "__main__":
+    run_app(app_factory(), host="0.0.0.0", port=8081)
--- src/mysql.py
+++ src/mysql.py
@@ -7,18 +7,52 @@ connection = None
 
 async def init_db():
     global connection
-    connection = pymysql.connect(host="35.205.33.242",
-                                 user="purple",
-                                 password="purple",
-                                 db="purple",
-                                 charset='utf8mb4')
+    connection = pymysql.connect(
+        host="35.205.33.242",
+        user="purple",
+        password="purple",
+        db="purple",
+        charset="utf8mb4",
+    )
 
 
 def insert_plant(name, nb_sunny_days, nb_rainy_days, watering_interval_days, thing_id):
     start_date = datetime.now().strftime("%Y-%m-%d")
     with connection.cursor() as cursor:
-        sql = 'INSERT INTO `plants` (name, nb_sunny_days, nb_rainy_days, watering_interval_days, start_date, thing_id) VALUES(%s,%s,%s,%s,%s,%s)'
-        cursor.execute(sql, (name, nb_sunny_days, nb_rainy_days, watering_interval_days, start_date, thing_id))
+        sql = "INSERT INTO `plants` (name, nb_sunny_days, nb_rainy_days, watering_interval_days, start_date, thing_id) VALUES(%s,%s,%s,%s,%s,%s)"
+        cursor.execute(
+            sql,
+            (
+                name,
+                nb_sunny_days,
+                nb_rainy_days,
+                watering_interval_days,
+                start_date,
+                thing_id,
+            ),
+        )
+        connection.commit()
+        cursor.close()
+
+
+def update_plant(
+    plant_id, name, nb_sunny_days, nb_rainy_days, watering_interval_days, thing_id
+):
+    start_date = datetime.now().strftime("%Y-%m-%d")
+    with connection.cursor() as cursor:
+        sql = "UPDATE `plants` SET name=%s, nb_sunny_days=%s, nb_rainy_days=%s, watering_interval_days=%s, start_date=%s, thing_id=%s WHERE id=%s"
+        cursor.execute(
+            sql,
+            (
+                name,
+                nb_sunny_days,
+                nb_rainy_days,
+                watering_interval_days,
+                start_date,
+                thing_id,
+                plant_id,
+            ),
+        )
         connection.commit()
         cursor.close()
 
@@ -43,7 +77,7 @@ def select_plants():
             "nb_rainy_days": plant[3],
             "watering_interval_days": plant[4],
             "start_date": plant[5].strftime("%Y-%m-%d"),
-            "thing_id": plant[6]
+            "thing_id": plant[6],
         }
         plants.append(obj)
 
@@ -68,12 +102,12 @@ def select_plant_by_id(id):
         "nb_rainy_days": result[3],
         "watering_interval_days": result[4],
         "start_date": result[5].strftime("%Y-%m-%d"),
-        "thing_id": result[6]
+        "thing_id": result[6],
     }
     return obj
 
 
-def del_plant_by_id(id):
+def delete_plant_by_id(id):
     result = None
     with connection.cursor() as cursor:
         sql = "SELECT `*` FROM plants WHERE id=%s"
@@ -92,7 +126,7 @@ def del_plant_by_id(id):
             "nb_rainy_days": deletedplant[3],
             "watering_interval_days": deletedplant[4],
             "start_date": deletedplant[5].strftime("%Y-%m-%d"),
-            "thing_id": deletedplant[6]
+            "thing_id": deletedplant[6],
         }
         return obj
     else:
@@ -101,7 +135,7 @@ def del_plant_by_id(id):
 
 def insert_thing(mac_address, location):
     with connection.cursor() as cursor:
-        sql = 'INSERT INTO `things` (mac_address, location) VALUES(%s, %s)'
+        sql = "INSERT INTO `things` (mac_address, location) VALUES(%s, %s)"
         cursor.execute(sql, (mac_address, location))
         connection.commit()
         cursor.close()
@@ -120,11 +154,7 @@ def select_things():
 
     things = []
     for thing in result:
-        obj = {
-            "id": thing[0],
-            "mac_address": thing[1],
-            "location": thing[2]
-        }
+        obj = {"id": thing[0], "mac_address": thing[1], "location": thing[2]}
         things.append(obj)
 
     return things
@@ -133,7 +163,7 @@ def select_things():
 def select_thing_by_id(id):
     result = None
     with connection.cursor() as cursor:
-        sql = 'SELECT `*` FROM things WHERE id=%s'
+        sql = "SELECT `*` FROM things WHERE id=%s"
         cursor.execute(sql, (id))
         result = cursor.fetchone()
         cursor.close()
@@ -141,18 +171,14 @@ def select_thing_by_id(id):
     if result == None:
         return result
 
-    obj = {
-        "id": result[0],
-        "mac_address": result[1],
-        "location": result[2]
-    }
+    obj = {"id": result[0], "mac_address": result[1], "location": result[2]}
     return obj
 
 
 def select_properties():
     result = None
     with connection.cursor() as cursor:
-        sql = 'SELECT `*` FROM properties'
+        sql = "SELECT `*` FROM properties"
         cursor.execute(sql)
         result = cursor.fetchall()
         cursor.close()
@@ -168,7 +194,7 @@ def select_properties():
             "characteristic": property[2],
             "type": property[3],
             "unit": property[4],
-            "readOnly": property[5]
+            "readOnly": property[5],
         }
         properties.append(obj)
     return properties
@@ -177,7 +203,7 @@ def select_properties():
 def select_property_by_name(name):
     result = None
     with connection.cursor() as cursor:
-        sql = 'SELECT `*` FROM properties WHERE name=%s'
+        sql = "SELECT `*` FROM properties WHERE name=%s"
         cursor.execute(sql, (name))
         result = cursor.fetchone()
         cursor.close()
@@ -191,6 +217,6 @@ def select_property_by_name(name):
         "characteristic": result[2],
         "type": result[3],
         "unit": result[4],
-        "readOnly": result[5]
+        "readOnly": result[5],
     }
     return obj
